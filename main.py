4c29d0c8389cd1f22bc616afdd9280ef103cf52a
giaaentrada06 <entrada.gd@obmontessori.edu.ph>
2026-01-06 08:28:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+
+email = input('Enter Email')
+password = input('Enter password:')
+
+if email == 'entrada.gd@obmontessori.edu.ph' and password == '123456':
+   print("log in successful")
+else:
+   print('incorrect email or password')
