8a51a8174b4d28439d572f70c42dc50aee063626
giaaentrada06 <entrada.gd@obmontessori.edu.ph>
2026-01-06 08:36:45 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-
-email = input('Enter Email')
-password = input('Enter password:')
-
-if email == 'entrada.gd@obmontessori.edu.ph' and password == '123456':
-   print("log in successful")
-else:
-   print('incorrect email or password')
